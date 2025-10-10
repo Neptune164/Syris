6eee1a87fecac307332b3353c223550e9c005039
Neptunr164 <179183669+Neptune164@users.noreply.github.com>
2025-10-10 17:53:11 -0400

    Revision on lag features and initialize test model

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -1,25 +1,33 @@
 import pandas as pd
 import os
 
-def build_lag_feature(file=None, start="2025-01-01", end="2025-12-31"):
+def build_lag_feature(file=None, start=None, end=None):
     if file is None:
         raise ValueError("The file is invalid.")
+    
     cleaned_data = pd.read_csv(file)
     cleaned_data['datetime']=pd.to_datetime(cleaned_data['datetime'],errors='coerce')
-    start = pd.to_datetime(start)
-    end = pd.to_datetime(end)
-    mask = (cleaned_data['datetime']>=start) & (cleaned_data['datetime']<end)
-    cleaned_data = cleaned_data[mask]
+
+    if start is None:
+        start = cleaned_data['datetime'].iloc[0]
+
+    if end is None:
+        end = cleaned_data['datetime'].iloc[-1]
+
+    s = pd.to_datetime(start)
+    e = pd.to_datetime(end)
+    mask = (cleaned_data['datetime']>=s) & (cleaned_data['datetime']<e)
+    filters = cleaned_data[mask]
 
     lag = pd.DataFrame()
     
     features = ['snowdepth', 'tempmin', 'tempmax', 'solarradiation', 'solarenergy', 'sealevelpressure']
     for col in features:
         for i in range(3):
-            lag[f'{col}_lag{i+1}'] = cleaned_data[col].shift(periods=i+1)
+            lag[f'{col}_lag{i+1}'] = filters[col].shift(periods=i+1)
     
-    lag_data = pd.concat(objs=[cleaned_data,lag], axis=1)
+    lag_data = pd.concat(objs=[filters,lag], axis=1)
     lag_data.dropna(inplace=True)
     
-    lag_data.to_csv(os.path.join("./data/lag_features/", f"{start.date()}_{end.date()}_weather_lagged.csv"), index=False)
+    lag_data.to_csv(os.path.join("./data/lag_features/", f"{s.date()}_{e.date()}_weather_lagged.csv"), index=False)
     return lag_data
--- src/test.py
+++ src/test.py
@@ -1,16 +1,41 @@
 import joblib
 import os
+import pandas as pd
 from feature_engineering import build_lag_feature
 from data_processing import preprocessing
 
 # load models
 rf_model = "./models/rf_100_20_1.0.pkl"
 xgb_model = "./models/xgboost_100_3_1.0.pkl"
+
 dirFile = "./data/raw/Syracuse_last15days.csv"
 tarPath = "./data/cleaned"
+
+cleaned_data = "./data/cleaned/09-24_10-10_weather_cleaned.csv"
+start_date = "2025-09-24"
+end_date = "2025-09-30"
+
+lag_feature = "./data/lag_features/2025-09-24_2025-09-30_weather_lagged.csv"
 filename = "09-24_10-10"
 
 RF = joblib.load(os.path.join(rf_model))
 GB = joblib.load(os.path.join(xgb_model))
 
-preprocessing(os.path.join(dirFile), os.path.join(tarPath), filename)
+# preprocessing(os.path.join(dirFile), os.path.join(tarPath), filename)
+# build_lag_feature(os.path.join(cleaned_data), start_date, end_date)
+
+date = []
+df = pd.read_csv(os.path.join(lag_feature))
+df['datetime'] = pd.to_datetime(df['datetime'])
+latest = df['datetime'].iloc[-1]
+lag = df.drop(columns=['datetime', 'temp'])
+
+preds = GB.predict(lag) + RF.predict(lag)
+for i in range(3):
+    date.append(latest+pd.Timedelta(days=i+1))
+
+result = pd.DataFrame({
+    "datetime": date,
+    "temperature": preds
+})
+print(result)
