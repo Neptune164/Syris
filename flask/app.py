c512ccd9dc6a01b403ffb720eee00b02a3d49661
Neptunr164 <179183669+Neptune164@users.noreply.github.com>
2025-10-10 19:02:35 -0400

    Backend completes

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask,jsonify
+import json
+
 
 app = Flask(__name__)
 
 @app.route("/predict", methods=["GET"])
 def display():
-    result = {
-        "status": "successful"
-    }
-    return result
+    with open("result.json") as f:
+        data = json.load(f)
+
+    return jsonify(data)
