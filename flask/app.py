b8083e464d588357cdb8f976667382fc64fc409f
Neptunr164 <179183669+Neptune164@users.noreply.github.com>
2026-06-12 17:21:51 +0800

    Fix bug on 404 not found.

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
+import json
+import os
 
 app = Flask(__name__)
 
-@app.route("/predict", methods=["GET"])
+@app.route("/predict", methods=["GET", "POST", "OPTIONS"])
 def display():
-    result = {
-        "status": "successful"
-    }
-    return result
+    filepath = os.path.join(os.path.dirname(__file__), "result.json")
+    with open(filepath) as f:
+        data = json.load(f)
+
+    return jsonify(data)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=5000,debug=False)
